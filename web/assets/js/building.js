375d8dce2ec1e8cffbd3bf14a773e21bc73f5f0a
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2017-05-01 13:11:36 +0200

    List of registered names for each rfid

--- web/assets/js/building.js
+++ web/assets/js/building.js
@@ -3,16 +3,33 @@
 d3.text("assets/data/building_map.txt",function(error, map){
 	if(error) console.log(error);
 	
-	var aMap = map.split('\n').map(function(d){ return d.trim().split(' ').map(function(e){return +e})});
-	
-	
-	console.log(aMap);
-	
+	var aMap = map.split('\n').map(function(d){ return d.trim().split(' ').map(function(e){return +e})});	
 	var building = buildingBitmap();
 	
 	d3.select("#building").call(building);
-	
 	building.data(aMap);
+	
+});
+
+d3.tsv("assets/data/rfid_assignments.txt",
+function(row){
+	var entries = d3.values(row);
+
+	if(entries[0].length > 0)
+		return {id: +entries[0],
+			person: entries[1]}
+},
+function(error,ids){
+	if(error) console.log(error);
+	
+	console.log("ids",ids);
+	
+	var rlist = rfidList();
+	d3.select("#persons")
+		.datum(ids)
+	.call(rlist);
+
+	
 	
 });
 
@@ -33,9 +50,16 @@ d3.tsv("assets/data/rfid_pathway.txt",
 			.key(function(d){return d.person})
 		.entries(paths);
 		
-		var trs = d3.values(trajs).map(function(d){return {
+		var trs = d3.values(trajs).map(function(d){
+			var pl = d.values.map(function(p,i){
+				if(i==0) return 0;
+				return euclideanDistance(p, d.values[i-1])
+			});
+			return {
 			person: +d.key,
-			values: d.values.map(function(p){return {x:p.x, y:p.y}})
+			values: d.values.map(function(p){return {x:p.x, y:p.y}}),
+			path_length: pl.reduce(function(a,b){return a+b}, 0),
+			delta_s: pl
 		}})
 	
 		console.log("trajs",trajs);
@@ -55,7 +79,7 @@ d3.tsv("assets/data/rfid_pathway.txt",
 		var path = d3.svg.line()
 			.x(function(d){return x(d.x)})
 			.y(function(d){return y(d.y)})
-		.interpolate("linear");
+		.interpolate("basis");
 		
 		
 		var g = d3.select('#building')
@@ -72,10 +96,25 @@ d3.tsv("assets/data/rfid_pathway.txt",
 			.attr("fill","none")
 			.attr("opacity",0.4)
 		.attr("d", path);
+		
+		
+		console.log(distances(trs[10],trs[50]));
 	}
 )
 
 
+function euclideanDistance(a,b){
+	return Math.sqrt(Math.pow(a.x -b.x,2) + Math.pow(a.y - b.y,2));
+}
+
+
+function distances(p1,p2){
+	console.log("p1",p1);
+	console.log("p2",p2);
+	return p1.values.map(function(d,i){
+		return [i, euclideanDistance(d, p2.values[i])]
+		});
+}
 
 
 function buildingBitmap(){
@@ -118,7 +157,6 @@ function buildingBitmap(){
 		
 	}
 	
-	
 	// setter and getter
 	me.height = function(_){
 		if(!arguments.length) return height;
@@ -134,6 +172,24 @@ function buildingBitmap(){
 		return me;
 	}
 	
-	
 	return me;
 }
+
+
+function rfidList(){
+	
+	function me(selection){
+		console.log(selection);
+		selection.append("ul")
+			.selectAll("li")
+			.data(function(d){return d})
+			.enter()
+			.append("li")
+			.text(function(d){return d.id+": " + d.person})
+			.on("click",function(d){console.log(d)})
+	}
+	
+	return me;
+	
+}
+
