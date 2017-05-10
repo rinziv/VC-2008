dd7de83cfa549e191898eb1e59bc122ac82f00be
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2017-05-10 23:36:44 +0200

    Creation of toolbar to select Year and RecordType

--- web/assets/js/migrants.js
+++ web/assets/js/migrants.js
@@ -21,9 +21,9 @@ function app(){
 					year: +d.EncounterDate.split('-')[0]
 				}
 		
-				if(d.EncounterCoords)
+				// if(d.EncounterCoords)
 					r['EncounterCoords'] = [+d.EncounterCoords[0],+d.EncounterCoords[1]];
-				if(d.LaunchCoords)
+				// if(d.LaunchCoords)
 					r['LaunchCoords'] = [+d.LaunchCoords[0],+d.LaunchCoords[1]];
 		
 				return r;
@@ -31,6 +31,8 @@ function app(){
 			
 			console.log("migrants", migrants);
 			console.log(migrants.length);
+			
+			// transform reports to a FeatureCollection
 			var fcReports = {
 				type:"FeatureCollection",
 				features: migrants
@@ -89,19 +91,55 @@ function app(){
 			.call(map);
 			
 			// change color of points
-			
 			// according to year
 			var colorYear = d3.scale.ordinal()
 				.domain([2005,2006,2007])
 				.range(colorbrewer['Dark2'][3]);
-			
-			
-			console.log([colorYear(2005), colorYear(2006), colorYear(2007)]);
-			
 			gReports.selectAll("path")
 			.attr("fill", function(d){return colorYear(d.properties.year)});
 			
+			createToolbar(migrants);
 		})
+	}
+	
+	
+	function createToolbar(migrants){
+		var toolbar = d3.select("#toolbar");
+		
+		
+		// create a selector for Years
+		toolbar.append("label")
+		.text("Years:");
+		
+		var tbYear = toolbar.append("div")
+			.attr({id:"mode-group", class:"btn-group", "data-toggle":"buttons" })
+			.selectAll("button")
+			.data([2005,2006,2007])
+			.enter()
+			.append("button")
+			.attr({class:"btn btn-group btn-default", role:"group"})
+			// .append("input")
+// 			.attr({type:"radio", name:"mode", id:"option1"})
+			.text(function(d){return d})
+			.on("click", function(d){console.log("click year", d)});
+		
+		toolbar.append("label")
+		.text("RecordType:");
+				
+		var tbRecordType = toolbar.append("div")
+			.attr({id:"mode-group", class:"btn-group", "data-toggle":"buttons" })
+			.selectAll("button")
+			.data(["All","Intediction","Landing"])
+			.enter()
+			.append("button")
+			.attr({class:"btn btn-group btn-default", role:"group"})
+			// .append("input")
+// 			.attr({type:"radio", name:"mode", id:"option1"})
+		.text(function(d){return d})
+		.on("click", function(d){console.log("click type", d)});
+		
+		// create a selector for RecordType
+		
 	}
 	
 	return me;
