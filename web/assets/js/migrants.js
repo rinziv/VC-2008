6bdf05d25ad2fc217459530cdfa17f67d6aaa84a
rinziv <sax@epimelesi.isti.cnr.it>
2017-05-10 15:44:10 +0200

    Map colors to years

--- web/assets/js/migrants.js
+++ web/assets/js/migrants.js
@@ -33,7 +33,8 @@ function app(){
 			console.log(migrants.length);
 			var fcReports = {
 				type:"FeatureCollection",
-				features: migrants.map(function(d,i){  // for each entry in Museums dictionary
+				features: migrants
+				.map(function(d,i){  // for each entry in Museums dictionary
 					if(d.EncounterCoords)
 						return {
 							type:"Feature",
@@ -56,17 +57,19 @@ function app(){
 			};
 			
 			
+			// dynamic computation of centroid
 			var extentX = d3.extent(migrants, function(d){return d.EncounterCoords[0]});
 			var extentY = d3.extent(migrants, function(d){return d.EncounterCoords[1]});
 			console.log("extentX", extentX);
 			console.log("extentY", extentY);
 			var centroid = [(extentX[0]+extentX[1])/2,(extentY[0]+extentY[1])/2];
 			console.log("centroid", centroid);
-			map.center(centroid)
-				.scale(2500);
 			
+			map.center(centroid)
+				.scale(4500);
+							
 			svg = selection.append("svg")
-			.attr({width:"100%", height:600});
+			.attr({width:"100%", height:800});
 			
 			var gWorld = svg.append("g")
 			.attr("class","mapWorld");
@@ -80,10 +83,24 @@ function app(){
 			
 			
 			
-			svg.append("g")
-				.attr("class","reports		")
+			var gReports = svg.append("g")
+				.attr("class","reports")
 				.datum(fcReports)
 			.call(map);
+			
+			// change color of points
+			
+			// according to year
+			var colorYear = d3.scale.ordinal()
+				.domain([2005,2006,2007])
+				.range(colorbrewer['Dark2'][3]);
+			
+			
+			console.log([colorYear(2005), colorYear(2006), colorYear(2007)]);
+			
+			gReports.selectAll("path")
+			.attr("fill", function(d){return colorYear(d.properties.year)});
+			
 		})
 	}
 	
