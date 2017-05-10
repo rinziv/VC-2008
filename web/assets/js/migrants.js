46d479349be7b349ff9be17cae9a99da65d7fd7d
rinziv <sax@epimelesi.isti.cnr.it>
2017-05-10 15:31:03 +0200

    Visualization of world map and position of encounters

--- web/assets/js/migrants.js
+++ web/assets/js/migrants.js
@@ -2,6 +2,9 @@
 
 function app(){
 	
+	var svg;
+	var map = MapWithLayers();
+	
 	function me(selection){
 		d3.json("assets/data/migrant.json",function(error, json){
 			if(error) throw error;
@@ -14,7 +17,8 @@ function app(){
 					RecordNotes: d.RecordNotes,
 					RecordType: d.RecordType,
 					USCG_Vessel: d.USCG_Vessel,
-					VesselType: d.VesselType
+					VesselType: d.VesselType,
+					year: +d.EncounterDate.split('-')[0]
 				}
 		
 				if(d.EncounterCoords)
@@ -26,11 +30,61 @@ function app(){
 			})
 			
 			console.log("migrants", migrants);
+			console.log(migrants.length);
+			var fcReports = {
+				type:"FeatureCollection",
+				features: migrants.map(function(d,i){  // for each entry in Museums dictionary
+					if(d.EncounterCoords)
+						return {
+							type:"Feature",
+							properties:{
+								EncounterDate: d.EncounterDate,
+								NumDeaths: +d.NumDeaths,
+								Passengers: +d.Passengers,
+								RecordNotes: d.RecordNotes,
+								RecordType: d.RecordType,
+								USCG_Vessel: d.USCG_Vessel,
+								VesselType: d.VesselType,
+								year: d.year
+							},
+							geometry:{
+								type:"Point",
+								coordinates: d.EncounterCoords
+							}
+						}
+				})
+			};
+			
+			
+			var extentX = d3.extent(migrants, function(d){return d.EncounterCoords[0]});
+			var extentY = d3.extent(migrants, function(d){return d.EncounterCoords[1]});
+			console.log("extentX", extentX);
+			console.log("extentY", extentY);
+			var centroid = [(extentX[0]+extentX[1])/2,(extentY[0]+extentY[1])/2];
+			console.log("centroid", centroid);
+			map.center(centroid)
+				.scale(2500);
+			
+			svg = selection.append("svg")
+			.attr({width:"100%", height:600});
+			
+			var gWorld = svg.append("g")
+			.attr("class","mapWorld");
+			
+			d3.json("assets/data/world.geojson", function(error, world){
+				console.log("world", world)
+				
+				gWorld.datum(world)
+				.call(map);
+			})
+			
+			
+			
+			svg.append("g")
+				.attr("class","reports		")
+				.datum(fcReports)
+			.call(map);
 		})
-		
-		
-		
-		
 	}
 	
 	return me;
