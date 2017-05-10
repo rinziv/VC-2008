b0cf94506494d3177497d37f54e5b62ee194d8b6
rinziv <sax@epimelesi.isti.cnr.it>
2017-05-10 14:56:51 +0200

    Preprocessing and exploration of input data

--- web/assets/js/migrants.js
+++ web/assets/js/migrants.js
@@ -5,7 +5,27 @@ function app(){
 	function me(selection){
 		d3.json("assets/data/migrant.json",function(error, json){
 			if(error) throw error;
-			console.log("migrants", json);
+			console.log("raw data", json);
+			var migrants = json.map(function(d,i){
+				var r =  {
+					EncounterDate: d.EncounterDate,
+					NumDeaths: +d.NumDeaths,
+					Passengers: +d.Passengers,
+					RecordNotes: d.RecordNotes,
+					RecordType: d.RecordType,
+					USCG_Vessel: d.USCG_Vessel,
+					VesselType: d.VesselType
+				}
+		
+				if(d.EncounterCoords)
+					r['EncounterCoords'] = [+d.EncounterCoords[0],+d.EncounterCoords[1]];
+				if(d.LaunchCoords)
+					r['LaunchCoords'] = [+d.LaunchCoords[0],+d.LaunchCoords[1]];
+		
+				return r;
+			})
+			
+			console.log("migrants", migrants);
 		})
 		
 		
