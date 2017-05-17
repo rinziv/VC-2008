025172c5263ea32c45ac9f856637670df9657ebd
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2017-05-17 11:46:35 +0200

    Create visualization of building map

--- web/assets/js/building-main.js
+++ web/assets/js/building-main.js
@@ -4,7 +4,21 @@ function BuildingApp(){
 	function me(selection){
 		console.log("BuildingApp");
 		
-
+		d3.text("assets/data/building_map.txt",function(error, map){
+			if(error) console.log(error);
+	
+			var aMap = map.split('\n') // split by line
+				.map(function(d){ 
+					return d.trim().split(' ') // split by space
+					.map(function(e){return +e}) // convert character to number
+				});	
+			var building = buildingBitmap();
+	
+			d3.select("#building")
+			.datum(aMap)
+			.call(building);
+	
+		});
 		
 		
 	}
--- web/assets/js/building.js
+++ web/assets/js/building.js
@@ -117,63 +117,6 @@ function distances(p1,p2){
 }
 
 
-function buildingBitmap(){
-	var width = 910;
-	var height = 610;
-	var svg;
-	
-	
-	function me(selection){
-		svg = selection
-			.append("svg")
-			.attr("width", width)
-		.attr("height", height);
-	}
-	
-	me.data = function(_){
-		if(!arguments.length) return data;
-		
-		// data is expected as an array of array
-		//first dimension: create the rows
-		var gr = svg.selectAll("g.row")
-			.data(_)
-			.enter()
-			.append("g")
-			.classed("row",true)
-		.attr("transform",function(d,i){return "translate(0,"+(i*10)+")"});
-		
-		var wallColor = d3.scale.ordinal()
-			.domain([0,1])
-		.range(["white", "lightgray"]);
-		
-		gr.selectAll("rect")
-			.data(function(d){return d})
-			.enter()
-			.append("rect")
-			.attr("x",function(d,i){return i*10})
-			.attr("width",10)
-			.attr("height",10)
-			.attr("fill",function(d){return wallColor(d)});
-		
-	}
-	
-	// setter and getter
-	me.height = function(_){
-		if(!arguments.length) return height;
-		height = _;
-		
-		return me;
-	}
-	
-	me.width = function(_){
-		if(!arguments.length) return width;
-		width = _;
-		
-		return me;
-	}
-	
-	return me;
-}
 
 
 function rfidList(){
