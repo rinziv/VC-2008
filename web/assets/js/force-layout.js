4d6bd52ae58d57e3cd469d182979681eaf187017
rinziv <rinzivillo@isti.cnr.it>
2017-05-26 13:31:00 +0200

    Highlight ego network from dblclick

--- web/assets/js/force-layout.js
+++ web/assets/js/force-layout.js
@@ -1,6 +1,8 @@
 function ForceLayout(){
 	var width = 900;
 	var height = 900;
+	
+	var graph;
   var link; 
   var node; 
    
@@ -9,20 +11,21 @@ function ForceLayout(){
   var force = d3.layout.force()
 	  .gravity(0.3) 
   	.linkDistance(150)
-  .charge(-100)
+  .charge(function(d){return d.highlight?-400: -100})
   ; 
   
   var linkStrokeScale = d3.scale.linear()
   .range([0,20]);
    
   function me(selection){ 
-     
+	  graph = selection.datum();
+	 
 	 linkStrokeScale
-	  	.domain(d3.extent(selection.datum().links, function(l){return l.weight}))
+	  	.domain(d3.extent(graph.links, function(l){return l.weight}))
 	 
 	 
-    force.nodes(selection.datum().nodes) 
-      .links(selection.datum().links) 
+    force.nodes(graph.nodes) 
+      .links(graph.links) 
       .size([width,height]) 
     .start() 
      
@@ -34,24 +37,27 @@ function ForceLayout(){
     link = svg.append("g") 
       .classed("links",true) 
       .selectAll(".link") 
-      .data(svg.datum().links) 
+      .data(graph.links) 
       .enter() 
       .append("path") 
       .classed("link", true) 
-      .style("stroke","lightgray") 
+      // .style("stroke","lightgray") 
     .style("stroke-width", function(d){return linkStrokeScale(d.weight)}); 
  
     node = svg.append("g") 
       .classed("nodes", true) 
       .selectAll(".node") 
-      .data(svg.datum().nodes) 
+      .data(graph.nodes) 
       .enter() 
       .append("circle") 
       .classed("node", true) 
     .attr("r", function(d){return d.weight})
-	.on("click", function(d){
+	.call(force.drag)
+	.on("dblclick", function(d){
+		console.log(d);
 		toggleNode(d);
-		tick();
+		force.alpha(0.3).start();
+		// tick();
 	}); 
      
      
@@ -59,8 +65,16 @@ function ForceLayout(){
   } 
   
   function toggleNode(n){
-	  n.highlight = !n.highlight;
-  }
+	n.highlight = !n.highlight;  
+  	var ts = n.neighbors.map(function(_){return graph.nodes[+_.key]});
+	ts.forEach(function(nb){
+		nb.highlight = n.highlight;
+		var tsn = nb.neighbors.map(function(_){return graph.nodes[+_.key]});
+		tsn.forEach(function(nb2){
+			nb2.highlight = n.highlight;
+		})
+	})
+}
    
   function tick(){ 
 	  link.attr("d", function(d) { 
@@ -68,7 +82,8 @@ function ForceLayout(){
 	          dy = d.target.y - d.source.y, 
 	          dr = Math.sqrt(dx * dx + dy * dy); 
 	      return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y; 
-	      }); 
+	      })
+		  .classed("highlight", function(l){return l.source.highlight && l.target.highlight}); 
  
     node.attr("cx", function(d) { return d.x; }) 
       .attr("cy", function(d) { return d.y; })
