15912219a7cb9b58d0eaf533f451feb21fa52093
rinziv <rinzivillo@isti.cnr.it>
2017-05-26 11:43:59 +0200

    Introducing force layout

--- web/assets/js/cellphones.js
+++ web/assets/js/cellphones.js
@@ -2,6 +2,8 @@
 
 function CellPhonesApp(){
 	
+	var graph;
+	
 	function me(selection){
 		console.log("CellPhones App");
 		var timeFormat = d3.time.format("%Y%m%d %H%M");
@@ -22,22 +24,27 @@ function CellPhonesApp(){
 				console.log("calls", calls);
 				console.log("time extent", d3.extent(calls, function(d){return d.ts}));
 				
-				var nodes = d3.nest()
+				var graph = d3.nest()
 					.key(function(d){return d.source})
 					.key(function(d){return d.target})
 				.entries(calls);
 				
-				nodes = nodes.map(function(n){return {id:+n.key, weight: n.values.length, neighbors: n.values}}) 
+				nodes = graph.map(function(n){return {id:+n.key, weight: n.values.length, neighbors: n.values}}) 
 				console.log("nodes", nodes);
 				
 				var links = nodes.map(function(s){
 					return s.neighbors.map(function(t){
-						return {source: s, target: nodes[+t.key-1], weight: t.values.length};
+						return {source: s, target: nodes[+t.key], weight: t.values.length};
 					})
 				});
 				
 				links = d3.merge(links);
 				console.log("links",links);
+				
+				var forcelayout = ForceLayout();
+				d3.select("#network")
+					.datum({ nodes: nodes, links: links})
+				.call(forcelayout);
 			}
 		)
 		
---
+++ web/assets/js/force-layout.js
@@ -0,0 +1,60 @@
+function ForceLayout(){
+	var width = 900;
+	var height = 900;
+  var link; 
+  var node; 
+   
+  var svg; 
+   
+  var force = d3.layout.force() 
+  ; 
+   
+  function me(selection){ 
+     
+	 
+	 
+    force.nodes(selection.datum().nodes) 
+      .links(selection.datum().links) 
+      .size([width,height]) 
+    .start() 
+     
+    if(!svg){ 
+      svg = selection.append("svg") 
+      .attr({width:width, height:height}); 
+    } 
+     
+    link = svg.append("g") 
+      .classed("links",true) 
+      .selectAll(".link") 
+      .data(svg.datum().links) 
+      .enter() 
+      .append("line") 
+      .classed("link", true) 
+      .style("stroke","lightgray") 
+    .style("stroke-width", 1); 
+ 
+    node = svg.append("g") 
+      .classed("nodes", true) 
+      .selectAll(".node") 
+      .data(svg.datum().nodes) 
+      .enter() 
+      .append("circle") 
+      .classed("node", true) 
+    .attr("r", function(d){return d.weight}); 
+     
+     
+    force.on("tick", tick); 
+  } 
+   
+  function tick(){ 
+    link.attr("x1", function(d) { return d.source.x; }) 
+      .attr("y1", function(d) { return d.source.y; }) 
+      .attr("x2", function(d) { return d.target.x; }) 
+      .attr("y2", function(d) { return d.target.y; }); 
+ 
+    node.attr("cx", function(d) { return d.x; }) 
+      .attr("cy", function(d) { return d.y; }); 
+  } 
+   
+  return me; 
+}
